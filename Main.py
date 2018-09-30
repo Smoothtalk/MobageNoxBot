7cd380573d31b7580e4db1d7394a71807f5449ef
Smoothtalk <avinash1010@gmail.com>
2018-09-29 21:24:58 -0600

    Worked on getting coordinates working, +- 2 pixel error

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 import pywinauto
+import win32gui
 
 APP_PATH = "D:\\Program Files\\Nox\\bin\\Nox.exe"
 
 def getWindowObject(appPath):
     app = pywinauto.Application().connect(path=appPath)
-    window = app.top_window()
+    #window = app.top_window()
+    window = app.window(title_re="NoxPlayer.*")
     return window
 
 def bringAppToFront(appWindow):
@@ -14,5 +16,16 @@ def bringAppToFront(appWindow):
     else:
         pywinauto.win32functions.SetForegroundWindow(appWindow.wrapper_object()) #bring to front
 
+def getWindowDimensions(appWindow):
+    print (type(appWindow.rectangle()))
+    w = appWindow.rectangle().width()
+    h = appWindow.rectangle().height() - 30 #window element bar subtraction
+    x = appWindow.rectangle().left
+    y = appWindow.rectangle().top
+
+    print ("top left co-ords:" + str(x) + 'x' + str(y))
+    print ("size:" + str(w) + 'x' + str(h))
+
 noxWindowObject = getWindowObject(APP_PATH)
 bringAppToFront(noxWindowObject)
+getWindowDimensions(noxWindowObject)
